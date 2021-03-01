1152800cca2fffd6f399552f447db59894984b02
G0nZy <47307516+G0nZy@users.noreply.github.com>
2021-03-01 11:19:04 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,55 @@
+# coding=utf-8
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+# def print_hi(name):
+# Use a breakpoint in the code line below to debug your script.
+
+
+
+# Press the green button in the gutter to run the script.
+# if __name__ == '__main__':
+#     print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
+from turtle import *
+state = {'turn': 0}
+def spinner():
+    clear()
+    angle = state['turn']/10
+    right(angle)
+    forward(100)
+    dot(120, 'red')
+    back(100)
+    right(120)
+    forward(100)
+    dot(120, 'green')
+    back(100)
+    right(120)
+    forward(100)
+    dot(120, 'blue')
+    back(100)
+    right(120)
+    update()
+def animate():
+    if state['turn']>0:
+        state['turn']-=1
+
+    spinner()
+    ontimer(animate, 20)
+def flick():
+    state['turn']+=10
+
+setup(420, 420, 370, 0)
+hideturtle()
+tracer(False)
+width(20)
+onkey(flick, 'space')
+listen()
+animate()
+done()
